cef6b4839f841f12599e38af4bee46aaf2c71975
birdgg <birdeggegg@gmail.com>
2023-04-16 11:08:56 +0800

    :tada: decode text with selection

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,11 @@
-function polling() {
-  // console.log("polling");
-  setTimeout(polling, 1000 * 30);
-}
+chrome.contextMenus.create({
+  id: "decode_base64",
+  title: "Decode Base64",
+  contexts: ["selection"],
+});
 
-polling();
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "decode_base64" && tab && tab.id) {
+    chrome.tabs.sendMessage(tab.id, { action: "decode_base64" });
+  }
+});
--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,21 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
+function decodeAndReplaceBase64(selectedText: string) {
+  try {
+    const decodedText = atob(selectedText);
+    const range = window.getSelection()?.getRangeAt(0);
+    if (range) {
+      range.deleteContents();
+      range.insertNode(document.createTextNode(decodedText));
+    }
+  } catch (error) {
+    console.error("Error decoding Base64:", error);
+  }
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "decode_base64") {
+    const selectedText = window.getSelection()?.toString();
+    if (selectedText) {
+      decodeAndReplaceBase64(selectedText);
+    }
   }
 });
